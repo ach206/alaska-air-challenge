b139d2807526c41f43dfc38a4f08d8e96c7c42f0
Camille Hall <camille@creativeambition.net>
2019-06-07 00:15:25 -0700

    enabled hambuger btn click

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Header from './comps/Header';
 import SubMenu from './comps/SubMenu';
 
+function openMenu(e) {
+  console.log('it opened');
+}
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,7 @@ function App() {
       <div className="slides">
 
         <SubMenu cName="slide-top"/>
+        <i className="fas fa-bars fa-2x" onClick={openMenu}></i>
         <SubMenu cName="slide-middle"/>
         <SubMenu cName="slide-bottom"/>
       </div>
