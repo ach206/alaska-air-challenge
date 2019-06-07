f2d17f627eaa9d0db625e6f5e9e21b393258c248
Camille Hall <camille@creativeambition.net>
2019-06-07 00:22:17 -0700

    reformat es6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Header from './comps/Header';
 import SubMenu from './comps/SubMenu';
 
-function openMenu(e) {
-  console.log('it opened');
-}
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  openMenu = (e) => {
+    console.log('it opened');
+  }
+  render() {
+    return(
+
+      <div className="App">
       <Header/>
       <div className="slides">
 
         <SubMenu cName="slide-top"/>
-        <i className="fas fa-bars fa-2x" onClick={openMenu}></i>
+        <i className="fas fa-bars fa-2x" onClick={(e) => this.openMenu(e)}></i>
         <SubMenu cName="slide-middle"/>
         <SubMenu cName="slide-bottom"/>
       </div>
     </div>
-  );
+      )
 }
-
-export default App;
+}
