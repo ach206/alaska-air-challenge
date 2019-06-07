9a86e6ef267bca10627866488643f3d00598f346
Camille Hall <camille@creativeambition.net>
2019-06-07 02:14:26 -0700

    imported vertical nav bar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react'
 import './App.css';
 import Header from './comps/Header';
 import SubMenu from './comps/SubMenu';
+import VertNavBar from './comps/VertNavBar';
 
 export default class App extends Component {
+  state = {
+    mainSlideClicked: false,
+    topLevelCats: 
+    ["SCHEDULING & BIDDING",
+     "SAFETY", 
+     "TRAINING", 
+     "ADMINISTRATION", 
+     "HOTELS", 
+     "MY BASE"],
+     catsLevel2: [""],
+     catsLevel3: [""],
+
+  }
   openMenu = (e) => {
+    let slides = document.querySelector('.slides');
+    let vertNav = document.querySelector('.nav-container');
+    let topSlide = document.querySelector('.slide-top');
+    if (this.state.mainSlideClicked === false){
+      this.setState({mainSlideClicked: true});
+      // vertNav.style.position="relative";
+      // vertNav.style.left="auto";
+    } else {
+      this.setState({mainSlideClicked: false});
+      // vertNav.style.position="absolute";
+      // vertNav.style.left="-999px";
+    }
+  slides.classList.toggle("slides-clicked");
+  topSlide.classList.toggle("slide-top-clicked");
+  vertNav.classList.toggle("nav-container-visible");
     console.log('it opened');
   }
   render() {
@@ -12,11 +41,11 @@ export default class App extends Component {
 
       <div className="App">
       <Header/>
+        <i className="fas fa-bars fa-2x" onClick={(e) => this.openMenu(e)}></i>
+        <VertNavBar cName="main-nav" topLevel={this.state.topLevelCats}/>
       <div className="slides">
 
         <SubMenu cName="slide-top"/>
-        <i className="fas fa-bars fa-2x" onClick={(e) => this.openMenu(e)}></i>
-        <SubMenu cName="slide-middle"/>
         <SubMenu cName="slide-bottom"/>
       </div>
     </div>
