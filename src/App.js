23bbc98bec2f5da1188e2aa36fceee41c37b0bf1
Camille Hall <camille@creativeambition.net>
2019-06-07 20:15:48 -0700

    top and second menu transitions successful

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Header from './comps/Header';
 import SubMenu from './comps/SubMenu';
 import VertNavBar from './comps/VertNavBar';
-
+import SubMenuL3 from './comps/SubMenuL3';
+let slides = document.querySelector('.slides');
+let vertNav = document.querySelector('.nav-container');
+let topSlide = document.querySelector('.slide-top');
 export default class App extends Component {
   state = {
-    mainSlideClicked: false,
+    hamburgerClicked: false,
     topLevelCats: 
     ["SCHEDULING & BIDDING",
      "SAFETY", 
@@ -14,27 +17,41 @@ export default class App extends Component {
      "ADMINISTRATION", 
      "HOTELS", 
      "MY BASE"],
-     catsLevel2: [""],
-     catsLevel3: [""],
+    resetLayout: false,
 
   }
   openMenu = (e) => {
     let slides = document.querySelector('.slides');
     let vertNav = document.querySelector('.nav-container');
     let topSlide = document.querySelector('.slide-top');
-    if (this.state.mainSlideClicked === false){
-      this.setState({mainSlideClicked: true});
-      // vertNav.style.position="relative";
-      // vertNav.style.left="auto";
+    if (this.state.hamburgerClicked === false){
+      this.setState({hamburgerClicked: true});
+
     } else {
-      this.setState({mainSlideClicked: false});
-      // vertNav.style.position="absolute";
-      // vertNav.style.left="-999px";
+      this.setState({hamburgerClicked: false});
     }
-  slides.classList.toggle("slides-clicked");
-  topSlide.classList.toggle("slide-top-clicked");
-  vertNav.classList.toggle("nav-container-visible");
-    console.log('it opened');
+    slides.classList.toggle("slides-clicked");
+    topSlide.classList.toggle("slide-top-clicked");
+    vertNav.classList.toggle("nav-container-visible");
+    
+    if (this.state.resetLayout === true && this.state.hamburgerClicked === true) {
+      slides.classList.toggle("slides-clicked");
+      topSlide.classList.toggle("slide-top-clicked");
+      vertNav.classList.toggle("nav-container-visible");
+    }
+  }
+
+  resetMenuLayout = () => {
+    // let slides = document.querySelector('.slides');
+    // let topSlide = document.querySelector('.slide-top');
+    // if (this.state.hamburgerClicked === true){
+    //   slides.classList.toggle("slides-clicked");
+    //   topSlide.classList.toggle("slide-top-clicked");
+    // }
+    // console.log('it hits');
+    this.setState({resetLayout: true});
+    this.openMenu();
+
   }
   render() {
     return(
@@ -43,10 +60,9 @@ export default class App extends Component {
       <Header/>
         <i className="fas fa-bars fa-2x" onClick={(e) => this.openMenu(e)}></i>
         <VertNavBar cName="main-nav" topLevel={this.state.topLevelCats}/>
+      <SubMenuL3 cName="lower-level-menu"/>
       <div className="slides">
-
-        <SubMenu cName="slide-top"/>
-        <SubMenu cName="slide-bottom"/>
+        <SubMenu cName="slide-top" closeMenu={this.resetMenuLayout}/>
       </div>
     </div>
       )
